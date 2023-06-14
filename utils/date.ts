47c6323c7a516c1bb6e8cfd5068ede85cd445b20
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-06-13 17:57:54 -0700

    feat: add date utils

---
+++ utils/date.ts
@@ -0,0 +1,37 @@
+export type Day = `${number}-${number}-${number}`;
+
+export function full(day: string): string {
+  const [yyyy, mm, dd] = day.split("-").map(Number);
+
+  if (month === undefined) throw new Error(`Invalid day: ${day}`);
+
+  return new Date(yyyy, mm - 1, dd)
+    .toLocaleString("default", {
+      month: "long",
+      year: "numeric",
+      day: "numeric",
+    });
+}
+
+export function month(day: Day): string {
+  const [yyyy, month] = day.split("-", 2).map(Number);
+
+  if (month === undefined) throw new Error(`Invalid day: ${day}`);
+
+  return new Date(yyyy, month - 1)
+    .toLocaleString("default", { month: "short", year: "numeric" });
+}
+
+export function tryMonth(date: string) {
+  try {
+    return month(date as Day);
+  } catch {
+    return date;
+  }
+}
+
+export function year(day: Day): string {
+  const [yyyy] = day.split("-", 1);
+
+  return yyyy;
+}
