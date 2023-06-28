ef6e226a54490ef144750f555e031fb66960894c
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-06-27 21:07:47 -0700

    refactor: remove review image

--- routes/reviews.tsx
+++ routes/reviews.tsx
@@ -1,7 +1,6 @@
 import Page from "../components/Page.tsx";
 import { ReviewTile } from "../components/ReviewTile.tsx";
 import { Tag } from "../components/Tag.tsx";
-import { Pic } from "../components/pics.tsx";
 import { BOOKS } from "../data/book/index.ts";
 import { FILMS } from "../data/film/index.ts";
 import { GAMES } from "../data/game/index.ts";
@@ -11,12 +10,6 @@ import { TV } from "../data/tv/index.ts";
 export default function About() {
   return (
     <Page tab="Reviews">
-      <Pic.Prompted
-        color="soot"
-        src="/reviews.webp"
-        prompt="mdjrny-v4 style, a collage of movie posters, book covers, album covers, and video game covers"
-      />
-
       <blockquote>
         A backlog of hundreds of reviews are in the process of being migrated
         and revised.
