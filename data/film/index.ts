b598eda1435690c545d9742ba436654e449f0f05
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-07-03 01:28:33 -0700

    add(film): "One Cut of the Dead"

--- data/film/index.ts
+++ data/film/index.ts
@@ -5,16 +5,18 @@
 import { MediaService } from "../media.ts";
 import $0 from "./midsommar.tsx";
 import $1 from "./mother.tsx";
-import $2 from "./the-day-after.tsx";
-import $3 from "./cargo.tsx";
-import $4 from "./martyrs.tsx";
-import $5 from "./world-war-z.tsx";
+import $2 from "./one-cut-of-the-dead.tsx";
+import $3 from "./the-day-after.tsx";
+import $4 from "./cargo.tsx";
+import $5 from "./martyrs.tsx";
+import $6 from "./world-war-z.tsx";
 
 export const FILMS = new MediaService({
   "midsommar": $0,
   "mother": $1,
-  "the-day-after": $2,
-  "cargo": $3,
-  "martyrs": $4,
-  "world-war-z": $5
+  "one-cut-of-the-dead": $2,
+  "the-day-after": $3,
+  "cargo": $4,
+  "martyrs": $5,
+  "world-war-z": $6
 });
---
+++ data/film/one-cut-of-the-dead.tsx
@@ -0,0 +1,36 @@
+import { Tier } from "../../components/Tier.tsx";
+import { film } from "../films.ts";
+
+export default film(import.meta, {
+  name: "One Cut of the Dead",
+  rating: "S",
+  released: "2017-11-04",
+  reviewed: "2023-06-29",
+  img: {
+    desc: <em>"Action!!!"</em>,
+  },
+  tags: [
+    "Japanese",
+    "comedy",
+    "zombie",
+    "meta",
+  ],
+  content: (
+    <>
+      <p>
+        A self-aware, silly film with a highly technical gimmick which it boldly
+        transcends, revealing an immensely delightful and charming core
+        underneath. It is a single-watch movie, but that single watch is a must.
+        I was shocked to learn afterwards that the film was made with only $25k
+        and an unknown cast, going on to gross a much-deserved $30mil.
+      </p>
+
+      <p>
+        Technically, the first portion disengaged me a couple times, which would
+        push this down to an{" "}
+        <Tier.A />, however those very moments are explained and made very
+        intentional later on, absolving it. The <Tier.S /> is earned.
+      </p>
+    </>
+  ),
+});
