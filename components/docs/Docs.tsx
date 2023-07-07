632a606d2f5d1a43a79d373349443a8452e420e2
Patrick Keenan <patrick.keenan.fyi@gmail.com>
2023-07-07 12:46:15 -0700

    refactor: condense docs components into one file

--- components/docs/Docs.tsx
+++ components/docs/Docs.tsx
@@ -1,26 +1,26 @@
-import { ComponentChildren, JSX } from "preact";
-import { PropsDesc } from "./PropsDesc.tsx";
-import { Examples } from "./Examples.tsx";
+import type { ComponentChildren, JSX } from "preact";
 
-export type DocsProps<
-  P extends Record<string, unknown>,
-  E extends HTMLElement,
-> =
-  & JSX.HTMLAttributes<HTMLElement>
-  & {
-    component: (props: P) => JSX.Element;
-    desc: ComponentChildren;
-    props: Record<
-      Exclude<keyof P, keyof JSX.HTMLAttributes<E>>,
-      string | JSX.Element
-    >;
-    examples: JSX.Element | Array<[string | JSX.Element, JSX.Element]>;
-  };
+export declare namespace Docs {
+  export type Props<
+    P extends Record<string, unknown>,
+    E extends HTMLElement,
+  > =
+    & JSX.HTMLAttributes<HTMLElement>
+    & {
+      component: (props: P) => JSX.Element;
+      desc: ComponentChildren;
+      props: Record<
+        Exclude<keyof P, keyof JSX.HTMLAttributes<E>>,
+        string | JSX.Element
+      >;
+      examples: Array<[string | JSX.Element, JSX.Element]>;
+    };
+}
 
 export function Docs<
   P extends Record<string, unknown>,
   E extends HTMLElement = HTMLElement,
->({ component, desc, examples, props, ...rest }: DocsProps<P, E>) {
+>({ component, desc, examples, props, ...rest }: Docs.Props<P, E>) {
   return (
     <section {...rest}>
       <h3>
@@ -29,11 +29,26 @@ export function Docs<
 
       <p>{desc}</p>
 
-      <PropsDesc entries={props} />
+      <h4>Props</h4>
 
-      {Array.isArray(examples)
-        ? <Examples entries={examples} />
-        : <Examples children={examples} />}
+      <ul>
+        {Object.entries(props).map(([name, desc]) => (
+          <li>
+            <code>{name}</code> - {desc}
+          </li>
+        ))}
+      </ul>
+
+      <h4>Examples</h4>
+
+      <ul class="px-9 list-none">
+        {examples.map(([example, rendered]) => (
+          <li>
+            {typeof example === "string" ? <code>{example}</code> : example}
+            {rendered}
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
